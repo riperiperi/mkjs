3b47a86756c988e2e1068bcb4d41a1095c52ad92
RHY3756547 <rhy3756547@hotmail.com>
2017-04-28 05:35:26 +0100

    Initial Commit of Project Files
    - Everything you ever wanted and more. Host alongside "Mario Kart
    DS.nds" provided by yourself to mess with it.

---
+++ Code/Engine/storage/fileStore.js
@@ -0,0 +1,83 @@
+window.fileStore = new (function(){
+	var db;
+	var indexedDB;
+
+	this.requestGameFiles = requestGameFiles;
+
+	function requestGameFiles(callback) {
+		indexedDB = window.indexedDB
+                     || window.webkitIndexedDB
+                     || window.mozIndexedDB
+                     || window.shimIndexedDB;
+
+		var request = indexedDB.open("MKJS_DB", 1);
+		request.onerror = window.onerror;
+
+		request.onsuccess = function(event) {
+			db = event.target.result;
+			loadGameFiles(callback);
+		}
+
+		request.onupgradeneeded = function(event) {
+			db = event.target.result;
+			var objectStore = db.createObjectStore("files", { keyPath: "filename" });
+			objectStore.transaction.oncomplete = function(event) {
+				loadGameFiles(callback);
+			}
+		}
+	}
+
+	function loadGameFiles(callback) {
+		var transaction = db.transaction(["files"]);
+		var objectStore = transaction.objectStore("files");
+		var request = objectStore.get("mkds.nds");
+		request.onerror = function(event) {
+			alert("Fatal database error!");
+		};
+		request.onsuccess = function(event) {
+			if (request.result == null) downloadGame("Mario Kart DS.nds", callback);
+			else callback(request.result.data);
+		};
+	}
+
+	function downloadGame(url, callback) {
+		if (typeof url == "string") {
+			var xml = new XMLHttpRequest();
+			xml.open("GET", url, true);
+			xml.responseType = "arraybuffer";
+			xml.onload = function() {
+				storeGame(xml.response, callback);
+			}
+			xml.send();
+		} else {
+			alert("You need to supply MKJS with a Mario Kart DS ROM to function. Click anywhere on the page to load a file.")
+			fileCallback = callback;
+			document.getElementById("fileIn").onchange = fileInChange;
+			waitForROM = true;
+		}
+	}
+
+	function fileInChange(e) {
+		var bFile = e.target.files[0];
+		var bReader = new FileReader();
+		bReader.onload = function(e) {
+			waitForROM = false; //todo: verify
+			storeGame(e.target.result, fileCallback);
+		};
+		bReader.readAsArrayBuffer(bFile);
+	}
+
+	function storeGame(dat, callback) {
+		var transaction = db.transaction(["files"], "readwrite");
+		var objectStore = transaction.objectStore("files");
+		var request = objectStore.put({filename:"mkds.nds", data:dat});
+
+		request.onerror = function(event) {
+			alert("Failed to store game locally!");
+			callback(dat);
+		};
+		request.onsuccess = function(event) {
+			callback(dat);
+		};
+	}
+})();
---
+++ Code/Particles/itemboxShard.js
@@ -0,0 +1,39 @@
+//
+// itemboxShard.js
+//--------------------
+// by RHY3756547
+//
+
+window.ItemShard = function(scene, targ, model) {
+	var t = this;
+	t.update = update;
+	t.draw = draw;
+
+	t.time = 0;
+	t.pos = vec3.clone(targ.pos);
+	t.vel = vec3.add([], targ.vel, [(Math.random()-0.5)*5, Math.random()*7, (Math.random()-0.5)*5]);
+	t.dirVel = [(Math.random()-0.5), (Math.random()-0.5), (Math.random()-0.5)];
+	t.dir = [Math.random()*2*Math.PI, Math.random()*2*Math.PI, Math.random()*2*Math.PI];
+	t.scale = Math.random()+0.5;
+	t.scale = [t.scale, t.scale, t.scale];
+
+	function update(scene) {
+		vec3.add(t.pos, t.pos, t.vel);
+		vec3.add(t.vel, t.vel, [0, -0.17, 0]);
+		vec3.add(t.dir, t.dir, t.dirVel);
+
+		if (t.time++ > 30) scene.removeParticle(t);
+	}
+
+	function draw(view, pMatrix, gl) {
+		var mat = mat4.translate(mat4.create(), view, t.pos);
+			
+		mat4.rotateZ(mat, mat, t.dir[2]);
+		mat4.rotateY(mat, mat, t.dir[1]);
+		mat4.rotateX(mat, mat, t.dir[0]);
+
+		mat4.scale(mat, mat, vec3.scale([], t.scale, 16));
+		model.draw(mat, pMatrix);
+	}
+
+}
